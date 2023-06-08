6411ddd4548ee345b1d78b736662b6195bc66021
MarahBhoty <94001337+MarahBhoty@users.noreply.github.com>
2023-06-08 15:54:47 +0300

    players movements
    week 3+4

--- King of Boxes/Assets/PlayerMovement.cs
+++ King of Boxes/Assets/PlayerMovement.cs
@@ -13,6 +13,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+        }
+        if (Input.GetKey("up"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+        }
+        if (Input.GetKey("down"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
+        }
+
     }
 }
