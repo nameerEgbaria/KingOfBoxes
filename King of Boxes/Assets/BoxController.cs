7db62c765a6a57fb387440c1c39c3c78262bf30f
MarahBhoty <94001337+MarahBhoty@users.noreply.github.com>
2023-10-05 13:01:15 +0300

    we fix the problem with change color when the box is not on the target

--- King of Boxes/Assets/BoxController.cs
+++ King of Boxes/Assets/BoxController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BoxController : MonoBehaviour
 {
     private bool isOnTarget = false;
+    private bool hasChangedColor = false;
     private Color originalColor;
     private Rigidbody rb;
 
@@ -17,16 +18,38 @@ public class BoxController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        // Check if the box collided with the target
-        if (collision.gameObject.CompareTag("target"))
+        // Check if the box collided with the target and if its color has not been changed yet
+        if (collision.gameObject.CompareTag("target") && !hasChangedColor)
         {
             isOnTarget = true;
 
             // Change the box's color to green
             GetComponent<Renderer>().material.color = Color.green;
 
+            // Set the flag to true to indicate that the color has been changed
+            hasChangedColor = true;
+
             // Disable the Rigidbody to prevent further movement
             rb.isKinematic = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        // Check if the box is moving out of the target
+        if (collision.gameObject.CompareTag("target"))
+        {
+            // Reset the color to its original color only if it has been changed
+            if (hasChangedColor && isOnTarget == false)
+            {
+                GetComponent<Renderer>().material.color = originalColor;
+                hasChangedColor = false; // Reset the flag
+            }
+            isOnTarget = false;
+
+
+            // Enable the Rigidbody to allow movement again
+            rb.isKinematic = false;
         }
     }
 
