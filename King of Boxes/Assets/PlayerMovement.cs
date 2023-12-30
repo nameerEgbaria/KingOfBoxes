a25d268ff27acc4e05a7569bc7b885c134e12c2a
nameerEgbaria <92601140+nameerEgbaria@users.noreply.github.com>
2023-12-30 22:07:14 +0200

    sync is done
    now we have a multiplayer game and its synchronized

--- King of Boxes/Assets/PlayerMovement.cs
+++ King of Boxes/Assets/PlayerMovement.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rb;
     [SerializeField] float movementSpeed = 6f;
+    PhotonView view;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        view = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -18,28 +20,31 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        if(view.IsMine)
+        {
+            rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
-        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        }
 
-       // if (Input.GetKeyDown("space"))
-       // {
-       //     GetComponent<Rigidbody>().velocity = new Vector3(0, 6f, 0);
-       // }
+        // if (Input.GetKeyDown("space"))
+        // {
+        //     GetComponent<Rigidbody>().velocity = new Vector3(0, 6f, 0);
+        // }
         //if (Input.GetKey("up"))
         //{
-          //  GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 6f);
-      //  }
+        //  GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 6f);
+        //  }
         //if (Input.GetKeyDown("right"))
         //{
-         //   GetComponent<Rigidbody>().velocity = new Vector3(6f, 0, 0);
-       // }
-       // if (Input.GetKeyDown("left"))
+        //   GetComponent<Rigidbody>().velocity = new Vector3(6f, 0, 0);
+        // }
+        // if (Input.GetKeyDown("left"))
         //{
-          //  GetComponent<Rigidbody>().velocity = new Vector3(-6f, 0, 0);
+        //  GetComponent<Rigidbody>().velocity = new Vector3(-6f, 0, 0);
         //}
-      //  if (Input.GetKey("down"))
-       // {
-         //   GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -6f);
+        //  if (Input.GetKey("down"))
+        // {
+        //   GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -6f);
         //}
 
     }
