a0d80eac79c99ce62c969e9bfa2fa01ffe6343a3
nameerEgbaria <92601140+nameerEgbaria@users.noreply.github.com>
2023-12-31 00:34:03 +0200

    the camera view now are diffrent for each players

--- King of Boxes/Assets/CameraSwitcher.cs
+++ King of Boxes/Assets/CameraSwitcher.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 
 public class CameraSwitcher : MonoBehaviour
 {
     public Camera MainCamera;    // Reference to the main camera.
     public Camera Camera2D;  // Reference to the second camera.
+    public PhotonView view2;
     void Start()
     {
-        MainCamera.enabled = true;
-        Camera2D.enabled = false;
+        view2 = GetComponent<PhotonView>();
+        if (view2.IsMine)
+        {
+            MainCamera.enabled = true;
+            Camera2D.enabled = false;
+        }
+        else
+        {
+            MainCamera.enabled = false;
+            Camera2D.enabled = true;
+        }
+
 
     }
     void Update()
     {
         // Check if the "U" key is pressed.
-        if (Input.GetKeyDown(KeyCode.U))
-        {
+       // if (Input.GetKeyDown(KeyCode.U))
+       // {
             // Toggle the active state of the cameras.
-            MainCamera.enabled = !MainCamera.enabled;
-            Camera2D.enabled = !Camera2D.enabled;
-        }
+       //     MainCamera.enabled = !MainCamera.enabled;
+        //    Camera2D.enabled = !Camera2D.enabled;
+       // }
     }
 }
